5c1ae3a13759456886f2bb2378d8f8ec1e660732
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2023-01-12 18:32:53 -0500

    Initial commit

---
+++ app/src/main/java/com/raiden/aoc_normal/MainActivity.java
@@ -0,0 +1,24 @@
+package com.raiden.aoc_normal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_main);
+    }
+}
